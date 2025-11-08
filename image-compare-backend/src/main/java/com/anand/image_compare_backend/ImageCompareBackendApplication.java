c3479efcab7c0c0b0765bd24110a942342749b7f
Anand Yadav <anand93056ad@gmail.com>
2025-11-08 15:12:34 +0530

    chore: intial commit for backend

---
+++ image-compare-backend/src/main/java/com/anand/image_compare_backend/ImageCompareBackendApplication.java
@@ -0,0 +1,13 @@
+package com.anand.image_compare_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageCompareBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageCompareBackendApplication.class, args);
+	}
+
+}
---
+++ image-compare-backend/src/test/java/com/anand/image_compare_backend/ImageCompareBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.image_compare_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageCompareBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
